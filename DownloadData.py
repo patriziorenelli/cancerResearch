67321436b29fef1276ace94f78060304e3c52d97
Patrizio Renelli <patriziore.pr@gmail.com>
2023-11-06 01:07:31 +0100

    Finito processo di download dei dati
    Finito il processo di download dei dati e popolamento delle tabelle con i dati individuati

--- DownloadData.py
+++ DownloadData.py
@@ -1,13 +1,350 @@
 from DatabaseManager import *
-import params
+import params 
 import sys
 import requests
 import psycopg2
 import json
+import csv
 
+
+
+# Funzione per scaricare e processare i dati di espressione da GDC
+'''
+Gli passo params in modo da potervi accedere 
+'''
+def download_and_process_expression_data(param):
+
+        # Creo connessione con il database 
+        cursor, conn = databaseConnection()
+        if cursor == None or conn == None:
+            print("Impossibile connettersi al database")
+            return 
+        else: 
+            print("Connessione riuscita")
+
+        # URL dell'API GDC per scaricare i file corrispondenti alle analisi
+        gdc_api_url = "https://api.gdc.cancer.gov/files"
+
+        # Download dei dati
+        filters = {
+            "op": "and",
+            "content": [
+                # Filtro riguardante il sito primario della malattia che vogliamo analizzare
+                {
+                    "op": "=",
+                    "content": {
+                        "field": "cases.primary_site",
+                        "value": "bronchus and lung"
+                    }
+                },
+                # Filtro riguardante il tipo di dati che vogliamo analizzare
+                {
+                    "op": "in",
+                    "content": {
+                        "field": "data_type",
+                        "value": ["Gene Expression Quantification"]   # clsProtein Expression Quantification
+                    }
+                },
+
+                # Filtro riguardante l'accesso dei dati
+                {
+                    "op": "=",
+                    "content": {
+                        "field": "access",
+                        "value": "open"
+                    }
+                },
+
+                # Filtro riguardante il formato del file su cui è riportata l'analisi
+                {
+                    "op": "=",
+                    "content": {
+                        "field": "data_format",
+                        "value": "TSV"
+                    }
+                }
+            ]
+        }
+
+        # Parametri usati per efettuare la get 
+        params = {
+            "filters": json.dumps(filters),
+            # Altri campi per ottenere maggiori informazioni sui file scaricati 
+            "fields": "file_name,file_size,created_datetime,updated_datetime,data_type,experimental_strategy,data_category,cases.project.project_id,cases.case_id,cases.submitter_id,associated_entities.entity_submitter_id",
+            "format": "JSON",
+            "size": "20",  # Numero massimo di file da scaricare per richiesta
+            "pretty": "true"  # pretty indica che la response viene formattata con spazi aggiuntivi per migliorare la leggibilità
+        }
+        
+        response = requests.get(gdc_api_url, params=params)
+        data = response.json()
+
+        # Elaborazione dei dati e inserimento nel database
+        for file_info in json.loads(response.content.decode("utf-8"))["data"]["hits"]:
+
+            file_id = file_info["id"]
+
+            # Gestione dei progetti 
+            project_id = file_info["cases"][0]["project"]["project_id"]
+            # Controllo se il progetto esiste già nel database 
+            if not checkExistProject(project_id, cursor): 
+                project_name = searchProject(project_id)
+                # Se ho recuperato informazioni sul progetto allora faccio l'insert nel database 
+                if project_name != None:
+                    insertNewProject(project_id, project_name, cursor, conn)
+
+            # Gestione dei case
+            if not checkExistCase(file_info["cases"][0]["submitter_id"], cursor):
+                case_data = searchCase(file_info["cases"][0]["case_id"])
+                if case_data != None:
+                    insertNewCase(case_data, project_id, cursor, conn)
+
+
+            type_id = None
+            # Gestione dei file 
+            if not checkExistFile(file_id, cursor):
+                type_id = insertNewAnalysis(file_id, file_info, project_id, cursor, conn)
+
+            
+
+                expression_data = downloadFile(file_id, type_id)
+                if expression_data == None:
+                    return 
+                
+
+                # FIN QUI TUTTO OK  
+
+
+                # VEDERE COME CAMBIARE QUESTA PARTE SOTTO E LA FUNZIONE  insertNewSamples
+                # MANCA UN DOPPIO TRY EXCEPT CHE COPRE IL CONTENUTO DI QUESTA FUNZIONE: UNO PER GLI ERRORI DEL DB E UNO PER GLI ERRORI GENERICI  -> COME HA FATTO VALERIO 
+
+# SELECT COUNT(*) FROM gene_expression_file -> 582308
+
+                for entity in file_info["associated_entities"]: 
+                        cursor.execute("INSERT INTO analysis_entity VALUES (%s, %s)", (file_id, entity["entity_submitter_id"]))
+                conn.commit()
+                    
+                if type_id == 1:
+                        for data_row in expression_data:
+                            # Inserimento dei dati di espressioni GENICHE nel database
+                            gene_id = data_row["gene_id"]
+                            stranded_first = data_row["stranded_first"]
+                            stranded_second = data_row["stranded_second"]
+
+                            cursor.execute("SELECT type_id FROM gene_type WHERE type = %s", (data_row["gene_type"],))
+                            gene_type_id = cursor.fetchone()[0]
+
+                            # Inserisci il gene nel database
+                            cursor.execute("INSERT INTO gene VALUES (%s, %s, %s) ON CONFLICT (gene_id) DO NOTHING;", (gene_id, data_row["gene_name"], gene_type_id))
+
+                            if stranded_first != 0 and stranded_second != 0: 
+                                cursor.execute("INSERT INTO gene_expression_file VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (file_id, gene_id, data_row["tpm_unstranded"], data_row["fpkm_unstranded"], data_row["fpkm_uq_unstranded"], data_row["unstranded"], stranded_first, stranded_second))
+                        conn.commit()
+                elif type_id == 2:
+                        # Inserimento dei dati di espressioni PROTEICHE nel database
+                        for data_row in expression_data:
+                            agid = data_row["AGID"]
+                            expression = data_row["protein_expression"]
+
+                            # Inserisci la proteina nel database
+                            cursor.execute("INSERT INTO protein VALUES (%s, %s, %s, %s, %s) ON CONFLICT (agid) DO NOTHING;", (agid, data_row["lab_id"], data_row["catalog_number"], data_row["set_id"], data_row["peptide_target"]))
+                            
+                            if expression != "NaN": cursor.execute("INSERT INTO protein_expression_file VALUES (%s, %s, %s)", (file_id, agid, expression))
+                        conn.commit()
+                print("File inserito nel database")
+            else: 
+                print("Il file è gia presente nel database")
+
+        print(f"Download, elaborazione e inserimento dei dati completati.")
+
+
+
+'''
+Verifica se esiste un progetto all'interno del database
+'''
+def checkExistProject(id, cursor):
+    cursor.execute("SELECT COUNT(*) FROM project WHERE project_id = %s", (id,))
+    result = cursor.fetchone()
+    if result[0] == 0: 
+        return False
+    return True
+
+'''
+Funzione che ricerca i dati di un singolo progetto
+'''
+def searchProject(id):
+    project_url = "https://api.gdc.cancer.gov/projects/" + id
+    params = {
+        #Puoi aggiungere altri campi che danno più info relative al progetto
+        "fields": "name",
+        "format": "JSON",
+        "pretty": "true"
+    }
+    response = requests.get(project_url, params=params)
+
+    if response.status_code == 200:
+        data = json.loads(response.content.decode("utf-8"))["data"]
+        return data["name"]
+    else:
+        print(f"Errore durante il download del progetto: {response.status_code}")
+        return None
+
+'''
+Funzione che inserisce i dati di un singolo nuovo progetto all'interno del database
+'''
+def insertNewProject(id, name, cursor,conn):
+    query = "INSERT INTO public.project VALUES ('{}', '{}') ON CONFLICT (project_id) DO NOTHING;".format(id,name)
+    cursor.execute(query)
+    conn.commit()
+
+'''
+Verifica se esiste un caso all'interno del database
+'''
+def checkExistCase(id, cursor):
+    cursor.execute("SELECT COUNT(*) FROM public.case WHERE case_id = %s", (id,))
+    result = cursor.fetchone()
+    if result[0] == 0: 
+        return False
+    return True
+
+'''
+Funzione che ricerca i dati di un singolo case
+'''
+def searchCase(id):
+    cases_url = "https://api.gdc.cancer.gov/cases/" + id
+
+    params = {
+        #Puoi aggiungere altri campi che danno più info relative al caso
+        "fields": "submitter_id,demographic.ethnicity,demographic.gender,demographic.race,demographic.vital_status,primary_site,disease_type,samples.submitter_id,samples.sample_type,samples.sample_type_id,samples.tumor_code,samples.tumor_code_id,samples.tumor_descriptor,samples.portions.submitter_id,samples.portions.analytes.submitter_id,samples.portions.analytes.concentration,samples.portions.analytes.aliquots.submitter_id,samples.portions.analytes.aliquots.concentration", #samples.portions.slides.submitter_id
+        "format": "JSON",
+        "pretty": "true"
+    }
+    response = requests.get(cases_url, params=params)
+
+    if response.status_code == 200:
+        data = json.loads(response.content.decode("utf-8"))["data"]
+        return data
+    
+    print(f"Errore durante il download del case: {response.status_code}")
+    return None 
+
+
+'''
+Funzione che inserisce i dati di un singolo nuovo case all'interno del database
+'''
+def insertNewCase(data, project_id, cursor, conn):
+    search_site_malattia = "SELECT site_id, disease_id FROM primary_site, disease WHERE LOWER(site) = LOWER('{}') AND LOWER(type) = LOWER('{}')"
+    insert_case = "INSERT INTO public.case VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}');"
+    
+    cursor.execute(search_site_malattia.format(data["primary_site"], data["disease_type"],))
+    disease_site = cursor.fetchone()
+
+    cursor.execute(insert_case.format(data["submitter_id"], data["demographic"]["ethnicity"], data["demographic"]["gender"], data["demographic"]["race"], data["demographic"]["vital_status"], project_id, disease_site[0], disease_site[1]))
+    conn.commit()
+    insertNewSamples(data["samples"], data["submitter_id"], cursor, conn)
+
+
+
+'''
+Funzione che inserisce i dati dei nuovi campioni all'interno del database
+'''
+def insertNewSamples(samples, case_id, cursor,conn):
+    inserisci_biospecie = "INSERT INTO biospecimen VALUES (%s, %s, %s)"
+    inserisci_tumore = "INSERT INTO tumor VALUES (%s, %s, %s) ON CONFLICT (tumor_code_id) DO NOTHING;"
+    inserisci_tipo_campione = "INSERT INTO sample_type VALUES (%s, %s) ON CONFLICT (type_id) DO NOTHING;"
+    inserisci_campione = "INSERT INTO sample VALUES (%s, %s, %s)"
+    inserisci_porzione = "INSERT INTO portion VALUES (%s, %s)"
+    inserisci_analita = "INSERT INTO analyte VALUES (%s, %s, %s)"
+    inserisci_aliquota = "INSERT INTO aliquote VALUES (%s, %s, %s)"
+
+    for sample in samples:
+        sample_id = sample["submitter_id"]
+        
+        if "tumor_code_id" in sample and sample["tumor_code_id"] != None: 
+            tumor_code = sample["tumor_code_id"]
+            cursor.execute(inserisci_tumore, (tumor_code, sample["tumor_code"], sample["tumor_descriptor"]))
+        else: tumor_code = None
+        if "sample_type_id" in sample and sample["sample_type_id"] != None: 
+            type_id = sample["sample_type_id"]
+            cursor.execute(inserisci_tipo_campione, (type_id, sample["sample_type"]))
+        else: type_id = None   
+
+        cursor.execute(inserisci_biospecie, (sample_id, case_id, 1))
+        cursor.execute(inserisci_campione, (sample_id, type_id, tumor_code))
+        if "portions" in sample:
+            for portion in sample["portions"]:
+                portion_id = portion["submitter_id"]
+
+                cursor.execute(inserisci_biospecie, (portion_id, case_id, 2))
+                cursor.execute(inserisci_porzione, (portion_id, sample_id))
+                if "analytes" in portion:
+                    for analyte in portion["analytes"]:
+                        analyte_id = analyte["submitter_id"]
+
+                        cursor.execute(inserisci_biospecie, (analyte_id, case_id, 3))
+                        cursor.execute(inserisci_analita, (analyte_id, portion_id, analyte["concentration"]))
+                        if "aliquots" in analyte:
+                            for aliquote in analyte["aliquots"]:
+                                aliquote_id = aliquote["submitter_id"]
+
+                                cursor.execute(inserisci_biospecie, (aliquote_id, case_id, 4))
+                                cursor.execute(inserisci_aliquota, (aliquote_id, analyte_id, aliquote["concentration"]))
+    conn.commit()
+
+'''
+Verifica se esiste un file all'interno del database
+'''
+def checkExistFile(id, cursor):
+    cursor.execute("SELECT COUNT(*) FROM analysis WHERE file_id = %s", (id,))
+    result = cursor.fetchone()
+    if result[0] == 0: 
+        return False
+    return True
+
+'''
+Funzione che inserisce i dati di un singolo nuovo file all'interno del database
+'''
+def insertNewAnalysis(file_id, file_info, project_id, cursor, conn):
+    cursor.execute("SELECT type_id, category_id, strategy_id FROM data_type, data_category, experimental_strategy WHERE type = %s AND category = %s AND strategy = %s", (file_info["data_type"], file_info["data_category"], file_info["experimental_strategy"],))
+    type_category_strategy_id = cursor.fetchone()
+    type_id = type_category_strategy_id[0]
+
+    # Inserisci i dettagli del file nel database
+    cursor.execute("INSERT INTO analysis VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (file_id, file_info["file_name"], file_info["file_size"], file_info["created_datetime"], file_info["updated_datetime"], project_id, type_id, type_category_strategy_id[1], type_category_strategy_id[2]))
+    conn.commit()           
+    return type_id
+
+
+'''
+Funzione per il download di un singolo file 
+'''
+def downloadFile(file_id, type_id):
+
+    file_url = "https://api.gdc.cancer.gov/data/" + file_id
+    response = requests.get(file_url)
+
+    if response.status_code == 200:
+        # Elabora i dati dal file scaricato
+        if type_id == 1: 
+            data = pd.read_csv(StringIO(response.text), sep="\t", comment="#", skiprows=[2,3,4,5])
+        elif type_id == 2: 
+            data = pd.read_csv(StringIO(response.text), sep="\t", comment="#")
+    
+        # Trasforma i dati in una lista di dizionari
+        expression_data = data.to_dict(orient="records")
+
+        return expression_data
+    else:
+        print(f"Errore durante il download del file: {response.status_code}")
+        return None
+
+'''
 cursor,conn = databaseConnection()
 if cursor == None:
     print("Errore nello stabilire la connessione col server")
     sys.exit()
 
-print("TUTTO OK :)")
+print("CONNESSIONE AL DATABASE STABILITA")
+
+'''
+download_and_process_expression_data(params)
--- params.py
+++ params.py
@@ -1,10 +1,11 @@
+# Parametri per la connessione al database PostgreSQL
 host = 'localhost'
 user = 'postgres'
 password = '1234'
 database = 'GDC'
 port = 5432
 
-# Parametri per la connessione al database PostgreSQL
+#Dizionario necessario per la connessione al database PostgreSQL
 db_params = {
     'host': host,    
     'database': database,      
@@ -12,3 +13,51 @@ db_params = {
     'password': password,     
     'port' :  port          
 }
+
+
+
+# Definizioni delle query SQL utilizzate nel codice
+cerca_progetto = "SELECT COUNT(*) FROM project WHERE project_id = %s"
+cerca_caso = "SELECT COUNT(*) FROM public.case WHERE case_id = %s"
+cerca_file = "SELECT COUNT(*) FROM analysis WHERE file_id = %s"
+cerca_tipo_categoria_strategia = "SELECT type_id, category_id, strategy_id FROM data_type, data_category, experimental_strategy WHERE type = %s AND category = %s AND strategy = %s"
+cerca_tipo_gene = "SELECT type_id FROM gene_type WHERE type = %s"
+
+inserisci_analisi = "INSERT INTO analysis VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+inserisci_entita_analisi = "INSERT INTO analysis_entity VALUES (%s, %s)"
+inserisci_espressione_genica = "INSERT INTO gene_expression_file VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+inserisci_gene = "INSERT INTO gene VALUES (%s, %s, %s) ON CONFLICT (gene_id) DO NOTHING;"
+inserisci_proteina = "INSERT INTO protein VALUES (%s, %s, %s, %s, %s) ON CONFLICT (agid) DO NOTHING;"
+inserisci_espressione_proteica = "INSERT INTO protein_expression_file VALUES (%s, %s, %s)"
+
+
+'''
+    Esempio di response ottenuto 
+{'data': {'hits': [
+    {'id': '6feef177-114c-4285-b242-481fe0aea551', 'cases': 
+        [{'case_id': 'aee86a89-0377-4080-b16c-408bfbe78687', 'submitter_id': 'TCGA-69-7980', 'project': {'project_id': 'TCGA-LUAD'}}], 
+        'associated_entities': [{'entity_submitter_id': 'TCGA-69-7980-01A-11R-2187-07'}], 
+        'file_name': '77322072-2a7f-49e6-a9d1-c521c25acd70.rna_seq.augmented_star_gene_counts.tsv', 'updated_datetime': '2022-01-19T14:20:39.180245-06:00', 'data_type': 'Gene Expression Quantification', 'data_category': 'Transcriptome Profiling', 'experimental_strategy': 'RNA-Seq', 'file_size': 4257905, 'created_datetime': '2021-12-13T19:35:41.947012-06:00'},
+    {'id': 'b9ac77d6-9a1b-443d-8ef5-e8ef2e3008dd', 'cases': 
+        [{'case_id': 'a41c46da-7ed4-4192-bd16-b3cbb94a5133', 'submitter_id': 'TCGA-55-1592', 'project': {'project_id': 'TCGA-LUAD'}}], 
+        'associated_entities': [{'entity_submitter_id': 'TCGA-55-1592-01A-01R-0946-07'}], 'file_name': '6f6bf240-1ca4-47d0-9a77-17b0ce9b8ae7.rna_seq.augmented_star_gene_counts.tsv', 'updated_datetime': '2022-01-19T14:21:07.893105-06:00', 'data_type': 'Gene Expression Quantification', 'data_category': 'Transcriptome Profiling', 'experimental_strategy': 'RNA-Seq', 'file_size': 4264851, 'created_datetime': '2021-12-13T19:55:53.312203-06:00'},
+    {'id': '3563bce2-6547-4081-883e-1e7e498c83be', 'cases': [{'case_id': 'ba49117e-dab6-4e7d-bc44-f11dcafa1318', 'submitter_id': 'TCGA-55-7727', 'project': {'project_id': 'TCGA-LUAD'}}], 
+        'associated_entities': [{'entity_submitter_id': 'TCGA-55-7727-01A-11R-2170-07'}], 'file_name': '59e8b7b7-5183-4655-aa5e-e4b5ba73eded.rna_seq.augmented_star_gene_counts.tsv', 'updated_datetime': '2022-01-19T14:20:16.513483-06:00', 'data_type': 'Gene Expression Quantification', 'data_category': 'Transcriptome Profiling', 'experimental_strategy': 'RNA-Seq', 'file_size': 4217198, 'created_datetime': '2021-12-13T20:00:15.260272-06:00'}, 
+    {'id': '68b00cb6-4335-4c64-a3e0-0c29ed574480', 'cases': [{'case_id': 'c4d1e105-28b7-48df-abef-fbe09782fdb2', 'submitter_id': 'TCGA-55-6972', 'project': {'project_id': 'TCGA-LUAD'}}], 
+        'associated_entities': [{'entity_submitter_id': 'TCGA-55-6972-01A-11R-1949-07'}], 'file_name': '5418228b-cfd0-4580-87c6-3ebf6282ad73.rna_seq.augmented_star_gene_counts.tsv', 'updated_datetime': '2022-01-19T14:20:37.428774-06:00', 'data_type': 'Gene Expression Quantification', 'data_category': 'Transcriptome Profiling', 'experimental_strategy': 'RNA-Seq', 'file_size': 4211769, 'created_datetime': '2021-12-13T19:48:35.287560-06:00'}, 
+    {'id': '55831884-c907-457d-bb2c-7c7b3f7c4cd1', 'cases': [{'case_id': 'bab43415-d413-40be-a4c0-2c40a52afe6a', 'submitter_id': 'TCGA-44-2668', 'project': {'project_id': 'TCGA-LUAD'}}], 
+        'associated_entities': [{'entity_submitter_id': 'TCGA-44-2668-11A-01R-1758-07'}], 'file_name': 'c7fd0bf8-753e-45d6-b987-51ec7559f728.rna_seq.augmented_star_gene_counts.tsv', 'updated_datetime': '2022-01-19T14:20:30.018854-06:00', 'data_type': 'Gene Expression Quantification', 'data_category': 'Transcriptome Profiling', 'experimental_strategy': 'RNA-Seq', 'file_size': 4237695, 'created_datetime': '2021-12-13T19:51:18.747467-06:00'}, 
+    {'id': '4089d037-ab25-47a6-be68-19742473cbc6', 'cases': [{'case_id': 'cd9e70e4-8622-4a07-8646-63f8275c1737', 'submitter_id': 'TCGA-49-AARE', 'project': {'project_id': 'TCGA-LUAD'}}], 
+        'associated_entities': [{'entity_submitter_id': 'TCGA-49-AARE-01A-11R-A41B-07'}], 'file_name': '50c308c9-922a-4083-ae09-e5e4d8c437af.rna_seq.augmented_star_gene_counts.tsv', 'updated_datetime': '2022-01-19T14:20:21.969629-06:00', 'data_type': 'Gene Expression Quantification', 'data_category': 'Transcriptome Profiling', 'experimental_strategy': 'RNA-Seq', 'file_size': 4242766, 'created_datetime': '2021-12-13T19:54:03.170411-06:00'}, 
+    {'id': 'db0caa6a-e863-4dad-a911-0c69127be9df', 'cases': [{'case_id': 'cc68632c-b1e3-491b-b562-9468e2d1c101', 'submitter_id': 'TCGA-86-7713', 'project': {'project_id': 'TCGA-LUAD'}}],
+        'associated_entities': [{'entity_submitter_id': 'TCGA-86-7713-01A-11R-2066-07'}], 'file_name': '55071ef3-bf09-43a2-a54c-a9ec61a84615.rna_seq.augmented_star_gene_counts.tsv', 'updated_datetime': '2022-01-19T14:21:20.189089-06:00', 'data_type': 'Gene Expression Quantification', 'data_category': 'Transcriptome Profiling', 'experimental_strategy': 'RNA-Seq', 'file_size': 4255626, 'created_datetime': '2021-12-13T19:31:57.981047-06:00'},
+    {'id': 'a0fce6cc-a525-4970-82b7-8512bb0708b3', 'cases': [{'case_id': 'c9c533ee-e154-4a56-bce9-b5af37574b2f', 'submitter_id': 'TCGA-55-7913', 'project': {'project_id': 'TCGA-LUAD'}}], 
+        'associated_entities': [{'entity_submitter_id': 'TCGA-55-7913-01B-11R-2241-07'}], 'file_name': '7d9c7c34-76e0-4622-b5f7-60e12f6d4e07.rna_seq.augmented_star_gene_counts.tsv', 'updated_datetime': '2022-01-19T14:20:58.441948-06:00', 'data_type': 'Gene Expression Quantification', 'data_category': 'Transcriptome Profiling', 'experimental_strategy': 'RNA-Seq', 'file_size': 4252453, 'created_datetime': '2021-12-13T19:51:58.058930-06:00'}, 
+    {'id': '38ec487d-0a4a-4287-9c14-4bfdb959b402', 'cases': [{'case_id': 'da345947-498b-4272-a231-a987ed4fe151', 'submitter_id': 'TCGA-44-A47G', 'project': {'project_id': 'TCGA-LUAD'}}],
+        'associated_entities': [{'entity_submitter_id': 'TCGA-44-A47G-01A-21R-A24H-07'}], 'file_name': '32675d71-6f79-4b8f-ab7c-e2350b15875c.rna_seq.augmented_star_gene_counts.tsv', 'updated_datetime': '2022-01-19T14:20:18.817877-06:00', 'data_type': 'Gene Expression Quantification', 'data_category': 'Transcriptome Profiling', 'experimental_strategy': 'RNA-Seq', 'file_size': 4235385, 'created_datetime': '2021-12-13T19:34:10.163354-06:00'}, 
+    {'id': '1ab1279d-7ccd-4450-8de8-9987bf6627dd', 'cases': [{'case_id': 'ccda26c1-a6d6-4317-8cf8-8a87e15ce12e', 'submitter_id': 'TCGA-44-2662', 'project': {'project_id': 'TCGA-LUAD'}}], 
+        'associated_entities': [{'entity_submitter_id': 'TCGA-44-2662-01B-02R-A277-07'}], 'file_name': '3d11905a-16eb-40b2-a36b-e37dca6359d3.rna_seq.augmented_star_gene_counts.tsv', 'updated_datetime': '2022-01-19T14:20:07.524745-06:00', 'data_type': 'Gene Expression Quantification', 'data_category': 'Transcriptome Profiling', 'experimental_strategy': 'RNA-Seq', 'file_size': 4225469, 'created_datetime': '2021-12-13T19:27:33.340965-06:00'}],
+         
+'pagination': {'count': 10, 'total': 1997, 'size': 10, 'from': 0, 'sort': '', 'page': 1, 'pages': 200}}, 'warnings': {}}
+
+'''
