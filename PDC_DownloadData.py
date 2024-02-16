8fdc11610c03a7095328d4b2c53d78aebf7f1898
Patrizio Renelli <patriziore.pr@gmail.com>
2024-02-16 01:36:56 +0100

    Add files via upload

--- PDC_DownloadData.py
+++ PDC_DownloadData.py
@@ -1,13 +1,8 @@
 from DatabaseManager import *
-import params 
-import sys
 import requests
-import psycopg2
 import json
-import csv
-
-
-
+from requests_html import *
+from DatabaseGenericInteraction import * 
 
 
 #  I dati su PDC girano intorno ai singoli studi, organizzando i dati in studi basati su su caratteristiche come proteoma o PTM e tipi sperimentali come senza etichetta o TMT 
@@ -50,12 +45,10 @@ def getPrograms():
     # FORSE BISOGNA FARE DIVERSE VOLTE LA QUERY E SALVARE IN STATUS I DATI PER RECUPERARE POI L'OFFSET DELLE QUERY
 
     programs = []
-        
     prog = json.loads(response.content.decode("utf-8"))
 
     if "data" not in prog and "allPrograms" not in prog["data"]:
         return 
-
 
     # Da qui possiamo prendere i programmi == project e tutti i studi associati ad ogni programma 
     for program in prog["data"]["allPrograms"]:
@@ -80,16 +73,10 @@ def getPrograms():
 
     return programs
 
-# GENE PRESENTE SIA IN PDC CHE IN GDC select * from gene, gene_type where  name = 'A1BG'and gene.type = type_id
-
-
-# FARE SAVE DEI STUDY ALL'INTERNO DELLA TABELLA PROJECT 
 '''
 Funzione che ottiene i case relativi ad un singolo programma
 '''
-def getCases(study):
-    cases = []
-
+def getCases(study, study_id, cursor, conn):
     # Otteniamo il numero di cases relativi allo studio
     gdc_api_url = 'https://pdc.cancer.gov/graphql?query={paginatedCaseDemographicsPerStudy (study_id: "' + study +  '" offset: 0 limit: 1 acceptDUA: true) { total caseDemographicsPerStudy { case_id case_submitter_id disease_type primary_site demographics { demographic_id ethnicity gender demographic_submitter_id race cause_of_death days_to_birth days_to_death vital_status year_of_birth year_of_death age_at_index premature_at_birth weeks_gestation_at_birth age_is_obfuscated cause_of_death_source occupation_duration_years country_of_residence_at_enrollment} } pagination { count sort from page total pages size } }}'
     response = requests.get(gdc_api_url)
@@ -110,80 +97,77 @@ def getCases(study):
         return
 
     for case in json.loads(response.content.decode("utf-8"))["data"]["paginatedCaseDemographicsPerStudy"]["caseDemographicsPerStudy"]:
-        #print(case)
-        # FARE CHECK SU DISEASE NEL DB E EVENTUALE INSERT 
-        # QUI PRENDERE L'ID DEL PRIMARY SITE E DEL DISEASE SE SONO GIA' INSERITI NELLA TABELLA SE NO FACCIAMO INSERT ( FUNZIONI PRESENTI NEL DatabaseGenericInteraction )
-        # FARE INSERT DEL SINGOLO CASE 
-        pass 
+            if not checkExistCase(case['case_submitter_id'],cursor):
+                if 'demographics' in case:
+                    disease = getDisease(case['disease_type'], cursor, conn)
+                    primary_site = getPrimarySite( case['primary_site'], cursor, conn)
+                    sub_id = case['case_submitter_id']
+                    case = case['demographics'][0]
+                    insertNewCase(sub_id, case['ethnicity'], case['gender'], case['race'], case['vital_status'], study_id, primary_site, disease, cursor, conn)
 
-    return cases
-
-def getSample(study):
-    # CONNESSIONE COL DB 
-    cursor, conn = databaseConnection()
-
-    #print(study)
+'''
+Funzione che ottiene tutti i campioni biologici di un singolo studio
+'''
+def getSample(study, cursor, conn):
     gdc_api_url = 'https://pdc.cancer.gov/graphql?query={ paginatedCasesSamplesAliquots(study_id:"' + study +  '"  offset:0 limit: 1 acceptDUA: true) { total casesSamplesAliquots { case_id case_submitter_id days_to_lost_to_followup disease_type index_date lost_to_followup primary_site samples { sample_id sample_submitter_id sample_type sample_type_id gdc_sample_id gdc_project_id biospecimen_anatomic_site composition current_weight days_to_collection days_to_sample_procurement diagnosis_pathologically_confirmed freezing_method initial_weight intermediate_dimension longest_dimension method_of_sample_procurement pathology_report_uuid preservation_method sample_type_id shortest_dimension time_between_clamping_and_freezing time_between_excision_and_freezing tissue_type tumor_code tumor_code_id tumor_descriptor diagnoses{ diagnosis_id diagnosis_submitter_id annotation} aliquots { aliquot_id aliquot_submitter_id analyte_type aliquot_run_metadata { aliquot_run_metadata_id label experiment_number fraction replicate_number date alias analyte} } } } pagination { count sort from page total pages size } } }'
     response = requests.get(gdc_api_url)
     samples_number = str( json.loads(response.content.decode("utf-8"))["data"]["paginatedCasesSamplesAliquots"]["total"] )
 
-
     gdc_api_url = 'https://pdc.cancer.gov/graphql?query={ paginatedCasesSamplesAliquots(study_id:"' + study +  '"  offset:0 limit: ' + samples_number + ' acceptDUA: true) { total casesSamplesAliquots { case_id case_submitter_id days_to_lost_to_followup disease_type index_date lost_to_followup primary_site samples { sample_id sample_submitter_id sample_type sample_type_id gdc_sample_id gdc_project_id biospecimen_anatomic_site composition current_weight days_to_collection days_to_sample_procurement diagnosis_pathologically_confirmed freezing_method initial_weight intermediate_dimension longest_dimension method_of_sample_procurement pathology_report_uuid preservation_method sample_type_id shortest_dimension time_between_clamping_and_freezing time_between_excision_and_freezing tissue_type tumor_code tumor_code_id tumor_descriptor diagnoses{ diagnosis_id diagnosis_submitter_id annotation} aliquots { concentration aliquot_id aliquot_submitter_id analyte_type concentration aliquot_run_metadata { aliquot_run_metadata_id label experiment_number fraction replicate_number date alias analyte} } } } pagination { count sort from page total pages size } } }'
     response = requests.get(gdc_api_url)
-    #print(json.loads(response.content.decode("utf-8")))
-
+    
     for samples in json.loads(response.content.decode("utf-8"))["data"]["paginatedCasesSamplesAliquots"]["casesSamplesAliquots"]:
         case_submitter_id = samples['case_submitter_id']
         for sample in samples["samples"]:
+            print("---------------")
             #print(sample)
-
-
             tumor_code = sample['tumor_code']
             tumor_code_id = sample['tumor_code_id']
             tumor_description = sample['tumor_descriptor']
-            # fare insert tumor 
-            #   inserisci_tumore = "INSERT INTO tumor VALUES (%s, %s, %s) ON CONFLICT (tumor_code_id) DO NOTHING;"
-            #   cursor.execute(inserisci_tumore, (tumor_code, sample["tumor_code"], sample["tumor_descriptor"]))
+
+            if  len(str(tumor_code_id)) != 0 and len(str(tumor_code)) != 0 and len(str(tumor_description)) != 0 and  str(tumor_code_id) != 'None' and str(tumor_code_id) != 'null' and not checkExistTumor(tumor_code_id, cursor):
+                    insertNewTumor(tumor_code_id, tumor_code, tumor_description, cursor, conn)
+            else:
+                tumor_code_id = None
 
             sample_type = sample['sample_type']
             sample_type_id = sample['sample_type_id']
-
-            #print( str(sample_type_id) + " "+ str(sample_type))
-
+        
             if sample_type_id == None:
-                q = "SELECT type_id FROM sample_type WHERE LOWER(type) = LOWER('{}')"
-                cursor.execute(q.format(sample_type))
-                sample_type_id =  cursor.fetchone()
-                sample_type_id = sample_type_id
+                sample_type_id = searchSampleTypeId(sample_type, cursor)
                 if sample_type_id == None:
                         continue
                 else:
                     sample_type_id = sample_type_id[0] 
-                    # fare insert usando sample_type_id
-            else:
-                # fare insert su sample_type con la cosa conflict
-                pass
+            elif not searchSampleTypeId(sample_type, cursor):
+                insertNewSampleType(sample_type_id, sample_type, cursor, conn)
 
-            # sample['sample_submitter_id'] può essere un'array quindi bisogna fare uno split su , e fare l'insert poer i singoli samp in sample['sample_submitter_id]
+            # sample['sample_submitter_id'] può essere un'array quindi bisogna fare uno split su , e fare l'insert per i singoli samp in sample['sample_submitter_id]
             for samp in sample['sample_submitter_id'].split(','):
                 sample_id =  samp
-                # fare insert sample 
-                # con fk sul tumor usare tumor_code_id
-                # con fk sul type usare sample_type_id
+                #print( sample_id, sample_type_id, tumor_code_id)
+                if not checkExistBiospecimen(sample_id, cursor):
+                    if checkExistCase(case_submitter_id, cursor):
 
+                        insertNewBiospecimen(sample_id, case_submitter_id, 1, cursor, conn)
+                        insertNewSample(sample_id, sample_type_id, tumor_code_id, cursor, conn)
+                        print(sample_id)
+                        for aliquote in sample['aliquots']:
+                            concentration = aliquote['concentration']
+                            aliquote_id = aliquote['aliquot_submitter_id']
+                            #print(aliquote)
+                            #print(sample_id, aliquote_id, concentration, str(concentration == None))
 
-                #print("\n")
-                # fai insert su biospecimen con id = sample_id e  case_submitter_id ed type = 1 
-                for aliquote in sample['aliquots']:
-                    concentration = aliquote['concentration']
-                    aliquote_id = aliquote['aliquot_submitter_id']
-                    # fai insert su biospecimen con aliquote_id e case = sample_id ed type= 4 
-                    # fai insert su aliquote con primary key = aliquote_id (IN PDC NON ABBIAMO UN ANALYTE_ID)  analyte_id = aliquote_id  e concentration  
-                #print(case_submitter_id,sample_id ,aliquote_id)
+                            # In PDC non abbiamo questi dati separati quindi l'unico modo per rispettare i vincoli imposti dal database per la parte di GDC è quella di creare portion e analyte fittizi se no ci basterebbe aliquote
+                            insertNewPortion(sample_id, sample_id, cursor, conn)
+                            insertNewAnalyte(sample_id, sample_id, concentration, cursor,conn)
 
+                            insertNewAliquote(sample_id, sample_id, concentration, cursor, conn)
 
+'''
+Funzione che ottiene i dati dei geni disponibili 
+'''
 def getGenes():
-
     # QUI BISOGNA CONSIDERARE L'OFFSET E I LIMIT DAL FILE status.txt
     gdc_api_url = 'https://pdc.cancer.gov/graphql?query={ getPaginatedGenes(offset: 0 limit: 100 acceptDUA: true) { total genesProper { gene_id gene_name NCBI_gene_id authority description organism chromosome locus proteins assays } pagination { count sort from page total pages size } } }'
     response = requests.get(gdc_api_url)
@@ -194,88 +178,85 @@ def getGenes():
 
     for gene in  json.loads(response.content.decode("utf-8"))["data"]["getPaginatedGenes"]["genesProper"]:
         gene_name = gene["gene_name"]
-        gdc_api_url= 'https://pdc.cancer.gov/graphql?query={geneSpectralCount (gene_name: "' + gene_name+ '" acceptDUA: true){gene_id gene_name NCBI_gene_id authority description organism chromosome locus proteins assays spectral_counts { project_submitter_id plex spectral_count distinct_peptide unshared_peptide study_submitter_id} }}'
+        #print(gene_name)
+
+        gdc_api_url= 'https://pdc.cancer.gov/graphql?query={geneSpectralCount (gene_name: "' + gene_name+ '" acceptDUA: true){gene_id gene_name NCBI_gene_id authority description organism chromosome locus proteins assays spectral_counts { project_submitter_id plex spectral_count distinct_peptide unshared_peptide study_submitter_id pdc_study_id} }}'
         response = requests.get(gdc_api_url)
         re = json.loads(response.content.decode("utf-8"))
+
+        
         if "data" not in re or "geneSpectralCount" not in re["data"]:
             break
         
         geneSpectralCount = re["data"]["geneSpectralCount"]
 
-        #  QUI OPERAZIONI SUI GENI 
-
-# ---------   TEST --------------------
-        
-        # FARE RICHIESTA CON NUOVA API INDIVIDUATA E PRENDERE TUTTI I DATI 
-        #gdc_api_url = 'http://rest.ensembl.org/lookup/symbol/homo_sapiens/' + 'ASMTL' + '?content-type=application/json'
-        #dati_gene_ens = json.loads(requests.get(gdc_api_url).content.decode("utf-8"))
-
-        #print(dati_gene_ens)
-
-    
-        # https://www.ncbi.nlm.nih.gov/datasets/docs/v2/reference-docs/rest-api/#post-/gene
-
-        # VEDERE SE E' POSSIBILE PRENDER INFORMAZIONI ANCHE SULLE PROTEINE 
-
-
-
-
-
-# ---------------- FINE TEST -------------------
-
-
-
-        gene_id = "ID"
-        # INSERT IN GENE usare gene_id, gene_name, type = DA PRENDERE NELL'API SOPRA DA FARE   E AGGIORNARE IL FILE status.txt
 
         for genSpec in geneSpectralCount:
-            if "spectral_counts" not in genSpec or "proteins" not in genSpec:
+            if "spectral_counts" not in genSpec or "proteins" not in genSpec or "gene_name" not in genSpec:
                 break
-
+            # PROTEINE ASSOCIATE AI GENE 
             proteins = (genSpec["proteins"]).split(";")
-            spectral_counts = genSpec["spectral_counts"]
 
-            for spec in spectral_counts:
-                if "study_submitter_id" not in spec or "spectral_count" not in spec or "distinct_peptide" not in spec or "unshared_peptide" not in spec:
-                   break 
-                study_submitter_id = spec["study_submitter_id"]
-                spectral_count = spec["spectral_count"]
-                distinct_peptide = spec["distinct_peptide"]
-                unshared_peptide = spec["unshared_peptide"]
-                # QUI FACCIAMO INSERT SULLA NUOVA TABELLA 
+            ncbi_gene_id = str( genSpec["NCBI_gene_id"])
+            gene_name = genSpec["gene_name"] 
+            #print(ncbi_gene_id)
+
+            headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+            json_data = {
+                'gene_ids': [
+                ncbi_gene_id
+                ]
+
+            }
+
+            # Effettuo la traduzione da gene_id NCBI in gede_id STANDARD Ensembl
+            gene_tr = json.loads( requests.post('https://api.ncbi.nlm.nih.gov/datasets/v2alpha/gene', headers=headers, json=json_data).content.decode("utf-8") )
+
+            if "reports" in gene_tr and len(gene_tr["reports"]) >=1 and "gene" in gene_tr["reports"][0] and "ensembl_gene_ids" in gene_tr["reports"][0]["gene"] and len(gene_tr["reports"][0]["gene"]["ensembl_gene_ids"]) >= 1 and ( (gene_tr["reports"][0]["gene"]["ensembl_gene_ids"])[0]).startswith('ENSG') and "type" in  gene_tr["reports"][0]["gene"]:
+                gene_id = (gene_tr["reports"][0]["gene"]["ensembl_gene_ids"])[0]
+                #print(gene_id, gene_name, gene_tr["reports"][0]["gene"]["type"], getGeneType(gene_tr["reports"][0]["gene"]["type"], cursor, conn))
+                insertNewGene(gene_id, gene_name, getGeneType(gene_tr["reports"][0]["gene"]["type"], cursor, conn), cursor, conn)
+
+                '''
+                In gene avremo comunque situazioni del genere:
+                ENSG00000109576.14   AADAT 1  -> Causato dalle transcription che sfrutta GDC 
+                ENSG00000109576    AADAT 1
+                '''
+
+                # QUI RICHIAMARE FUNZIONE DI SCRAPING WEB  PASSANDOGLI IL GENE NAME 
+                # GetGeneProInformation(gene_name, cursor, conn)
+
+                # QUI BISOGNEREBBE POI RIOTTENERE TUTTI I STUDI 
+
+
+            else:
+                pass
+        
     pass
 
 
-def sviluppo():            
+def sviluppo(cursor, conn):           
+
     programmi = getPrograms()
-    #print(len(programmi))
     for programma in programmi:
-        #print("PROGRAMMA: "+ programma.program_name + " ID: " + str(programma.program_id))
         for progetto in programma.projects:
-            #print(" PROGETTO: " + progetto.project_name + " ID: " + str(progetto.project_id) )
             for studio in progetto.studies: 
-                #print("     STUDIO: "+ studio.study_name + " ID: " + str(studio.study_id))
+                # Effettuiamo l'insert degli study che sono l'equivalente dei project all'interno di PDC 
+                if not checkExistProject(studio.study_submitter_id, cursor):
+                    insertNewProject(studio.study_submitter_id, studio.study_name, cursor, conn)
 
-                # QUI BISOGNA FARE INSERT PER LO STUDIO NELLA TABELLA PROJECT E DAL PRIMARY SITE DEL PROJECT FARE INSERT NELLA TABELLA PRIMARY_SITE
-                #getCases(studio.study_id)
-                #getSample(studio.study_id)
-
-                getGenes()
-
-                #print(studio.disease_types)
+                #getCases(studio.study_id, studio.study_submitter_id, cursor, conn)
+                #getSample(studio.study_id, cursor, conn)
+    getGenes()
 
 
+# Creo connessione con il database 
+cursor, conn = databaseConnection()
 
-
-'''
-QUERY DA VEDERE PER ANALYSIS
-select file_id, filename, file_size, created_datetime,updated_datetime, project, category, experimental_strategy.strategy_id, type
-from analysis join data_category on data_category.category_id = data_category 
-			  join  data_type on data_type.type_id = data_type
-			  join experimental_strategy on experimental_strategy.strategy_id = experimental_strategy
-'''
-
-sviluppo()
+sviluppo(cursor, conn)
 
 
 
